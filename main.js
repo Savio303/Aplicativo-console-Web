6d00bb099155f6d2449a782210812a390c4e24a8
Savio303 <105862904+Savio303@users.noreply.github.com>
2022-08-23 09:02:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("Meu primeiro console");
+
+var first_name = "Ranbir";
+var last_name = "Kappor";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a = 10;
+
+var b = 5;
+
+var add = a + b;
+var sub = a - b;
+var mul = a * b;
+console.log(add);
+console.log(sub);
+console.log(mul);
